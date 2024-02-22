c5e9440120af8ab40e8e19ab757a96ad739db0ef
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2024-02-22 14:55:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- QuickView Wrapper -->	
+<div class="quickview-wrapper"></div>
+<!-- QuickView Wrapper -->
+<!-- QuickView Loader -->
+<div class="quickview-loader">
+	<div class="loader"></div>
+	</div>
+<!-- QuickView Loader -->
+<!-- QuickView Overlay -->
+<div class="quickview-overlay"></div>
+<!-- QuickView Overlay -->
+
+<!-- QuickView Overlay Click-->
+<script type="text/javascript">
+    $('.quickview-overlay').on('click',function () {
+        quickView.closeButton();
+    });
+</script>
+
